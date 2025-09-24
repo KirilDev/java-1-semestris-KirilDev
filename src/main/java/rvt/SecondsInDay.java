e3bde0c41a478207c816df24efad354520ec1d84
KirilDev <alexprogrammer2023@gmail.com>
2025-09-24 14:01:23 +0300

    first realization of the SecondsInDay  On branch main  Changes to be committed: 	modified:   src/main/java/rvt/App.java 	modified:   src/main/java/rvt/SecondsInDay.java

--- src/main/java/rvt/SecondsInDay.java
+++ src/main/java/rvt/SecondsInDay.java
@@ -1,6 +1,17 @@
 package rvt;
 
-
+import java.util.Scanner;
 public class SecondsInDay {
-    
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the amount of the days: ");
+        int userResult = Integer.valueOf(scanner.nextLine());
+        scanner.close();
+        calculation(userResult);
+    }
+    public static void calculation(Integer amount){
+        int result = amount * 24 * 60 * 60;
+        System.out.println(result);
+    }
 }
