95155de7b4dba35ba8daa58347688e0a505fb081
KirilDev <alexprogrammer2023@gmail.com>
2025-10-08 14:51:05 +0300

    Sum of Sequence On branch main Changes to be committed: 	modified:   src/main/java/rvt/SumOfSequence.java

--- src/main/java/rvt/SumOfSequence.java
+++ src/main/java/rvt/SumOfSequence.java
@@ -4,6 +4,22 @@ import java.util.Scanner;
 
 public class SumOfSequence {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
         
+        System.out.println("First number? : ");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+
+
+        System.out.println("Second number? : ");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+
+        scanner.close();
+
+        for (int i = firstNumber; i <secondNumber; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is: " + sum);
     }
 }
