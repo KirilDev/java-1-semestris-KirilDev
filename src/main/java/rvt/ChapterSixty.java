a9c209a4ccf1a4daeb9bb687f41446a0a2a90c3a
KirilDev <alexprogrammer2023@gmail.com>
2025-10-23 13:16:41 +0300

    New File ChapterSixty Created  On branch main  Changes to be committed: 	new file:   src/main/java/rvt/ChapterSixty.java

---
+++ src/main/java/rvt/ChapterSixty.java
@@ -0,0 +1,22 @@
+package rvt;
+
+public class ChapterSixty {
+    public static void main(String[] args){
+        //TODO: https://chortle.ccsu.edu/javaLessons/chap60/progExercises60.html
+    }
+    public static void ex1(){
+
+    }
+    public static void ex2(){
+        
+    }
+    public static void ex3(){
+        
+    }
+    public static void ex4(){
+        
+    }
+     public static void ex5(){
+        
+    }
+}
