16803a8b319dfcea7d82e388dfb8f45ebbba23d0
KirilDev <alexprogrammer2023@gmail.com>
2025-11-06 15:50:36 +0200

    Creating new program - Students and Course

---
+++ src/main/java/rvt/Course.java
@@ -0,0 +1,33 @@
+package rvt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Course {
+    String courseName;
+    ArrayList<Student> students = new ArrayList<Student>();
+
+
+
+    void addStudent(){
+
+    }
+
+    void listStudents(){
+
+    }
+    void addGrade(){
+
+    }
+    void listGrades(){
+
+    }
+    public static void main(String[] args){
+        
+    }   
+}
+class Student {
+    String name;
+    int age;
+    ArrayList<Integer> grades = new ArrayList<Integer>();
+}
