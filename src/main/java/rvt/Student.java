27db899b3a87811f06dd1e82a657251e329bbdba
KirilDev <alexprogrammer2023@gmail.com>
2025-11-06 15:32:04 +0200

    Student.java fixes  On branch main  Changes to be committed: 	modified:   src/main/java/rvt/Student.java

--- src/main/java/rvt/Student.java
+++ src/main/java/rvt/Student.java
@@ -22,7 +22,7 @@ class Student {
 
     void showInfo(){
         System.out.println("Student's name: " + name);
-        System.out.println("Student's surname: " + name);
+        System.out.println("Student's surname: " + surname);
         System.out.println("Student's age: " + age);
         System.out.println("Student's currently studying: " + course);
         System.out.println("Student's current at " + courseNumber + " course");
@@ -31,24 +31,29 @@ class Student {
     }
 
     void suggestInternship(){
-        if (course == 3 && isExamsWritten == true) {
+        if (courseNumber == 3 && isExamsWritten == true) {
             System.out.println("Congrats on exams! You can get full-time internship to finish your education!");
         }
-        else if (course == 3 && isExamsWritten == false) {
+        else if (courseNumber == 3 && isExamsWritten == false) {
             System.out.println("Student can get part-time internship since he'll write exams soon...");
         }
         else{
-            System.out.println("You're not allowed to get internship");
+            System.out.println("You need to wait for a part-time intership.");
         }
     }
     public static void main(String[] args){
         Student a242400 = new Student("Kirill", "L.", "Computer Science", 2, 17, false, false );
-        Student a242100 = new Student("Akita", "Light", "Computer Science", 3, 19, false, true)
-
+        Student a242100 = new Student("Akita", "Light", "Computer Science", 3, 19, false, true);
+        Student a232100 = new Student("Aleksandr", "B.", "Polygraphics Department", 3, 19, false, false);
         a242400.showInfo();
         a242400.suggestInternship();
+        System.out.println(" ");
 
         a242100.showInfo();
         a242100.suggestInternship();
+        System.out.println(" ");
+
+        a232100.showInfo();
+        a232100.suggestInternship();
     }
 }
