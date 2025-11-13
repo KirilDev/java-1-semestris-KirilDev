0a54aed31789252003ee9a9ba430ea0d45784c35
KirilDev <alexprogrammer2023@gmail.com>
2025-11-13 12:40:55 +0200

    Importing the template

--- src/main/java/rvt/Statistics.java
+++ src/main/java/rvt/Statistics.java
@@ -1,5 +1,27 @@
 package rvt;
 
 public class Statistics {
-    
+    private int count;
+
+
+    public Statistics(){
+
+    }
+
+    public void addNumber(int number){
+
+    }
+
+    public int getCount(){
+
+    }
+
+    public static void main(String[] args){
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
+    }
 }
