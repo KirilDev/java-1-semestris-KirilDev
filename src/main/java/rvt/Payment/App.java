d232de8013af51ad28641a7ad516310f4e76dbbf
KirilDev <alexprogrammer2023@gmail.com>
2025-11-13 14:14:17 +0200

    Payment Card Updates

--- src/main/java/rvt/Payment/App.java
+++ src/main/java/rvt/Payment/App.java
@@ -2,24 +2,50 @@ package rvt.Payment;
 
 public class App {
     public static void main(String[] args) {
-        // PaymentCard card = new PaymentCard(50);
-        // System.out.println(card);
+        PaymentCard card = new PaymentCard(50);
+        System.out.println(card);
 
-        // card.eatHeartily();
-        // System.out.println(card);
+        card.eatHeartily();
+        System.out.println(card);
 
-        // card.eatHeartily();
-        // System.out.println(card);
+        card.eatHeartily();
+        System.out.println(card);
 
-        // card.addMoney(15);
-        // System.out.println(card);
+        card.addMoney(15);
+        System.out.println(card);
 
-        // card.addMoney(90.2);
-        // System.out.println(card);
+        card.addMoney(90.2);
+        System.out.println(card);
 
-        PaymentCard card = new PaymentCard(10);
-        System.out.println("Paul: " + card);
-        card.addMoney(-15);
-        System.out.println("Paul: " + card);
+        // PaymentCard card = new PaymentCard(10);
+        // System.out.println("Paul: " + card);
+        // card.addMoney(-15);
+        // System.out.println("Paul: " + card);
+
+
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+
+        System.out.println("Paul: " + paulsCard.toString());
+        System.out.println("Matt: " + mattsCard.toString());
+
+        paulsCard.addMoney(20);
+
+        mattsCard.eatHeartily();
+
+        System.out.println("Paul: " + paulsCard.toString());
+        System.out.println("Matt: " + mattsCard.toString());
+
+        paulsCard.eatAffordably();
+        mattsCard.eatAffordably();
+
+        mattsCard.addMoney(50);
+
+        System.out.println("Paul: " + paulsCard.toString());
+        System.out.println("Matt: " + mattsCard.toString());
+
     }
 }
