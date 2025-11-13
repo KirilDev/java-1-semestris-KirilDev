3417d5e9ed0da5cbbc2977c01b7de1bea66f2dbd
KirilDev <alexprogrammer2023@gmail.com>
2025-11-13 13:54:48 +0200

    Payment Card Task

---
+++ src/main/java/rvt/Payment/PaymentCard.java
@@ -0,0 +1,13 @@
+package rvt;
+
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        // write code here
+    }
+
+    public String toString() {
+        // write code here
+    }
+}
