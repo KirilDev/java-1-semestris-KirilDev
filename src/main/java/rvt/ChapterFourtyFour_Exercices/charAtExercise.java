419ead7337a741e10f51feab5c90a7398789231b
KirilDev <alexprogrammer2023@gmail.com>
2025-11-27 11:26:44 +0200

    Connecticum - Exercise 2 - charAt

---
+++ src/main/java/rvt/ChapterFourtyFour_Exercices/charAtExercise.java
@@ -0,0 +1,20 @@
+package rvt.ChapterFourtyFour_Exercices;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class charAtExercise {
+    public static void main(String[] args){
+       Scanner scanner = new Scanner(System.in);
+    
+       System.out.println("Enter a string: ");
+       String userInput = scanner.nextLine();
+
+       scanner.close();
+
+       for (int i = 0; i < userInput.length(); i++) {
+        System.out.println(userInput.charAt(i));
+       }
+    }
+}
